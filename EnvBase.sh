efea5c6bdf3fc5aa738dd32a42c18ede7643d283
Itai Danielli <Danielli.Itai@gmail.com>
2020-05-02 12:22:01 +0300

    current upgrade

--- EnvBase.sh
+++ EnvBase.sh
@@ -6,10 +6,18 @@ function LnxVersion()
 	lsb_release -a
 }
 
+function LnxUpdate()
+{
+	sudo apt-get update -y
+	sudo apt autoremove -y
+	sudo apt upgrade -y
+}
+
 #upgrade to latest release version(long term support).
 function LnxUpgrade()
 {
-	sudo apt-get update -y && sudo apt-get upgrade -y
+	sudo apt-get update -y
+	&& sudo apt-get upgrade -y
 	sudo apt install update-manager-core
 	sudo do-release-upgrade
 }
