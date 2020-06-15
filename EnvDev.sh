ec9c3d8056d6b56a4969391d315352ab1380588b
Itai Danielli <Danielli.Itai@gmail.com>
2020-06-15 08:27:25 +0300

    Check if Setup file exists.

--- EnvDev.sh
+++ EnvDev.sh
@@ -17,10 +17,19 @@ function GitClone()
 	cd $WORK_DIR
 	rm -rf $WORK_DIR/$1
 	git clone $GIT_USER_REPOS/$1.git
+	
+	_FILE=$WORK_DIR/$1/Setup.sh
+	if test -f "$_FILE"; then
+		echo "$_FILE exists."
+	fi
 }
 
 function EnvGitClone()
 {
+	git config --global user.name 'Danielli-Itai'
+	git config --global user.email 'Danielli.Itai@gmail.com'
+	git config --global user.password 'Guy1Alon2Maya3'
+	
 	GitClone _EnvLnx
 	GitClone NodeNlpServer
 }
