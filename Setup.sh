bd1d8093699df7dc3357e1b223c652f3510394ef
Itai Danielli <Danielli.Itai@gmail.com>
2020-11-27 14:48:06 +0200

    Set user global credentials

--- Setup.sh
+++ Setup.sh
@@ -5,13 +5,16 @@
 #Start from home directory
 cd ~/
 
-echo 'GitSetup <param1> function installs the git client.'
+echo 'GitSetup <param1> <param2> <param3> function installs the git client.'
 echo 'param1 - git user name to set the git user and cache git password for 1 hour.'
 function GitSetup()
 {
   sudo apt-get update
   sudo apt install git-all -y
 
-  sudo git config --global user.name "Danielli-Itai"
+  sudo git config --global user.name $1
+	sudo git config --global user.email $2
+	sudo git config --global user.password $3
+
   sudo git config --global credential.helper 'cache --timeout=3600'
 }
