7174bedfc47d16f5368c4ffd944c7e707dbfb685
Itai Danielli <Danielli.Itai@gmail.com>
2020-11-27 15:09:52 +0200

    Get git password from user

--- Setup.sh
+++ Setup.sh
@@ -5,16 +5,22 @@
 #Start from home directory
 cd ~/
 
-echo 'GitSetup <param1> <param2> <param3> function installs the git client.'
-echo 'param1 - git user name to set the git user and cache git password for 1 hour.'
+echo 'GitSetup <param1> <param2> function installs the git client.'
+echo 'param1 - git user name to set the git user'
+echo 'param2 - git user email address'
+# and cache git password for 1 hour.'
 function GitSetup()
 {
+  # Install Github.
   sudo apt-get update
   sudo apt install git-all -y
 
+  echo 'Git password ?'
+  read pass
+  # Set global credentials.
   sudo git config --global user.name $1
 	sudo git config --global user.email $2
-	sudo git config --global user.password $3
+	sudo git config --global user.password $pass
 
-  sudo git config --global credential.helper 'cache --timeout=3600'
+#  sudo git config --global credential.helper 'cache --timeout=3600'
 }
