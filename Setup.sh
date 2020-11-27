c17f4e38861ba07faf34e47688c79d93fbb772a7
Itai Danielli <Danielli.Itai@gmail.com>
2020-11-27 14:28:20 +0200

    GitSetup documentation.

--- Setup.sh
+++ Setup.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
 
 #	Install git client and clone the environment repository.
+
+#Start from home directory
 cd ~/
-sudo apt-get update
-sudo apt install git-all -y
-git clone https://github.com/Danielli-Itai/_EnvLnx
+
+echo 'GitSetup <param1> function installs the git client.'
+echo 'param1 - git user name to set the git user and cache git password for 1 hour.'
+function GitSetup()
+{
+  sudo apt-get update
+  sudo apt install git-all -y
+
+  sudo git config --global user.name "Danielli-Itai"
+  sudo git config --global credential.helper 'cache --timeout=3600'
+}
