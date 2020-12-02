612c71163f72a1c4684e83f6b2667ed6daa1f737
Itai Danielli <Danielli.Itai@gmail.com>
2020-12-02 13:26:41 +0200

    Move nodeclone to git

--- EnvNodeJs.sh
+++ EnvNodeJs.sh
@@ -10,22 +10,6 @@ function NodeInstall()
 }
 
 
-echo "Pull Python repository and Install."
-function NodeClone()
-{
-	# Pull latest version from Github.
-	echo "*** Cloning $1"
-	git clone https://github.com/Danielli-Itai/$1.git
-
-	# Move to folder and install all dependencies.
-	echo "Installing dependecies $1"
-	cd $1
-	sudo npm install
-
-	# Return to root folder.
-	cd ..
-}
-
 function PyRun()
 {
 	python3 $1
