16df7b522cd358ac2160d3fbad788b9a7fb7c640
Itai Danielli <Danielli.Itai@gmail.com>
2020-12-07 08:43:07 +0200

    undo python remove.

--- EnvPython.sh
+++ EnvPython.sh
@@ -12,10 +12,6 @@ function PythonInst()
 	sudo add-apt-repository ppa:deadsnakes/ppa
 	sudo apt install python3.8
 	sydo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.8
-	
-	sudo rm /usr/bin/python3
-	sudo ln -s python3.8 /usr/bin/python3
-
 	sudo apt install -y python3-pip
 	sudo python3 -m pip install --upgrade pip*
 	sudo apt-get install -y python3-tk
