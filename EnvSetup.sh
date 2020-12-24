e83d8173bc0dd9d34f560ed735af2eb955649bf6
Itai Danielli <Danielli.Itai@gmail.com>
2020-12-24 14:33:17 +0200

    add EnvSetup

---
+++ EnvSetup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#MySql environment entry.
+echo "Environment setup: EnvReset."
+
+
+
+
+
+
+function EnvReset()
+{
+	GitReset _EnvLnx
+}
--- EnvStart.sh
+++ EnvStart.sh
@@ -68,6 +68,7 @@ source ./_EnvLnx/EnvTools.sh
 source ./_EnvLnx/EnvPython.sh
 source ./_EnvLnx/EnvMySql.sh
 source ./_EnvLnx/EnvNodeJs.sh
+source ./_EnvLnx/EnvSetup.sh
 
 
 
